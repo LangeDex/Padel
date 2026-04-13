0c0150ee052dd99259cf31102f523a616e52a927
LangeDex <langskog.lang@gmail.com>
2026-04-13 11:03:36 +0200

    Add files via upload

--- Padel/src/main/java/com/Skoglund/Padel/config/SecurityConfig.java
+++ Padel/src/main/java/com/Skoglund/Padel/config/SecurityConfig.java
@@ -60,14 +60,12 @@ public class SecurityConfig {
 //
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 //import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 //import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 //import org.springframework.security.web.SecurityFilterChain;
 //
 //@Configuration
 //@EnableWebSecurity
-//@EnableMethodSecurity
 //public class SecurityConfig {
 //
 //    @Bean
